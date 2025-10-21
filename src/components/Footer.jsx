fb2fadf17d97d421f00aac3a3e02ed393eef27c7
Susan Gakii <susangakii71@gmail.com>
2025-10-21 09:43:18 +0300

    add header, footer, import them in home

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer>
+      <p className="copyright">&copy; 2025 DishCovery KE. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header className="header">
+      <h1>🍽️ DishCovery KE</h1>
+      <p>Find Your Next Bite, Fast</p>
+    </header>
+  );
+}
+
+export default Header;
